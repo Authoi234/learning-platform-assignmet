66748959795e7c980103ed347701e7e533ed2470
Authoi234 <jawadkhan.authoi@gmail.com>
2023-10-10 21:10:22 +0630

    some changes for looking nice

--- src/components/pages/Nav/Nav.js
+++ src/components/pages/Nav/Nav.js
@@ -8,13 +8,13 @@ const NavBar = () => {
             <Navbar collapseOnSelect expand="lg" style={{backdropFilter: 'blur(20px)'}} bg='light' className="bg-transparent fixed-top">
                 <Container>
                     <img width={'8%'} src={require('../../../assets/download1.png')} fluid alt='' />
-                    <Navbar.Brand className='text-light' href='https://learning-platform-assignmemt.web.app'>Authoi Education</Navbar.Brand>
+                    <Navbar.Brand href="https://learning-platform-assignmemt.web.app/" className='text-black fw-bold'>Authoi Education</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto align-items-center">
-                            <Link className="text-decoration-none text-light fs-5 me-3" to={'/courses'}>Courses</Link>
-                            <Link className="text-decoration-none text-light fs-5 me-3" >Blog</Link>
-                            <Link className="text-decoration-none text-light fs-5 me-3" >FAQ</Link>
+                            <Link className="text-decoration-none text-black fs-5 me-3" to={'/courses'}>Courses</Link>
+                            <Link className="text-decoration-none text-black fs-5 me-3" >Blog</Link>
+                            <Link className="text-decoration-none text-black fs-5 me-3" >FAQ</Link>
                         </Nav>
                         <Nav>
                             <Nav.Link href="#deets">More deets</Nav.Link>
