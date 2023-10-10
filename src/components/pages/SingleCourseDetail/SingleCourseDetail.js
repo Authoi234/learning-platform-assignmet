36409ed4f328aa2a1f60a22ef221a5c56faa347f
Authoi234 <jawadkhan.authoi@gmail.com>
2023-10-10 16:54:36 +0630

    courses detail pages is ready

--- src/components/pages/SingleCourseDetail/SingleCourseDetail.js
+++ src/components/pages/SingleCourseDetail/SingleCourseDetail.js
@@ -10,10 +10,10 @@ const SingleCourseDetail = () => {
                 <h1 className="btn-ghost normal-case text-5xl">{courseDetail.title}</h1>
             </div>
             <div className='lg:flex md:block'>
-                <iframe className='lg:w-4/6 w-full' height="300" src={courseDetail.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe className='lg:w-4/6 w-full' height="300" src={courseDetail.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 <div>
                     <h3 className=''>Learn from Video</h3>
-                    <p className='py-3 text-lg'>Learn {courseDetail.title} by video. In video our teachers teach you coding directly.After learning you should know basics of of previous courses</p>
+                    <p className='py-3 text-lg'>Learn {courseDetail.title} by video. In video our teachers teach you coding directly.After learning you should know basics of previous courses</p>
                 </div>
             </div>
             <div>
@@ -29,10 +29,18 @@ const SingleCourseDetail = () => {
                 <p className='py-3 text-2xl'>{courseDetail.howWorks}</p>
                 <img width={'100%'} src={courseDetail.workingImg} alt='' />
             </div>
-            <div>
+            <div className='bg-base-300'>
                 <h2 className='py-3 text-4xl'>Hello World</h2>
-                <div className='w-full h-full p-3'>
-                    
+                <div className='w-full h-full p-3 bg-black block'>
+                    <h3 className='text-gray-400 pr-10'>{courseDetail.helloWorld.line1}</h3>
+                    <h3 className='text-gray-400 pl-10'>{courseDetail.helloWorld.line2}</h3>
+                    <h3 className='text-gray-400 pr-10'>{courseDetail.helloWorld.line3}</h3>
+                </div>
+                <div>
+                    <span className='badge badge-primary'>OUTPUT</span>
+                    <div className='w-full h-full p-3 bg-white shadow-lg block'>
+                        <h1>Hello World !</h1>
+                    </div>
                 </div>
             </div>
             <div>
