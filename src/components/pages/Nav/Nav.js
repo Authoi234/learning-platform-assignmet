2a3961afe06cb11508301bc037f6e5f8229d197a
Authoi234 <jawadkhan.authoi@gmail.com>
2023-10-17 21:36:59 +0630

    toggle theme is added and website is ready

--- src/components/pages/Nav/Nav.js
+++ src/components/pages/Nav/Nav.js
@@ -1,21 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Container, Nav, Navbar, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { FaUserLarge } from "react-icons/fa6";
+import { FaUserLarge, FaSun, FaMoon } from "react-icons/fa6";
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const NavBar = () => {
+    const [theme, setTheme] = useState('Light');
     const { user, logOut } = useContext(AuthContext);
+
     const handleLogOut = () => {
         logOut()
             .then(() => { })
             .catch(e => console.log(e))
     }
+    
+    const toogleTheme = () => {
+        if (theme === 'Light') {
+            setTheme('Dark')
+        }
+        if(theme === 'Dark'){
+            setTheme('Light')
+        }
+    }
+
     const renderTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props}>
             {user.displayName}
         </Tooltip>
     );
+
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" style={{ backdropFilter: 'blur(20px)' }} bg='light' className="h-20 bg-transparent fixed-top">
@@ -30,10 +43,13 @@ const NavBar = () => {
                             <Link className="text-decoration-none text-black fs-5 me-3" >FAQ</Link>
                         </Nav>
                         <Nav>
+                            <div>
+                                <button className="btn mx-3" type="button" onClick={toogleTheme}>{theme === 'Dark' ? <FaSun className='fs-4'></FaSun> : <FaMoon className='fs-4'></FaMoon>}</button>
+                            </div>
                             {
                                 user ? <p className='d-flex align-items-center'>{user?.photoURL ? <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderTooltip}><img className='w-10' src={user.photoURL} alt='' /></OverlayTrigger> : <FaUserLarge></FaUserLarge>}  <Button onClick={handleLogOut} variant='outline-danger' className='fw-bold'>Log Out</Button></p> : <Button variant='outline-danger' href='/login' className='border d-flex text-primary fs-6 px-5 align-items-center'>Login</Button>
-                                }
-                                </Nav>
+                            }
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
